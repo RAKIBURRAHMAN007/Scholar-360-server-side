0efe8168f134710992fcc6ab0a7ddedb0c2b3682
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-01-21 00:14:50 +0600

    user delete api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -46,6 +46,16 @@ async function run() {
             const result = await userCollection.insertOne(user);
             res.send(result)
 
+        })
+        app.get('/users',async(req,res)=>{
+            const data = await userCollection.find().toArray();
+            res.send(data)
+        })
+        app.delete('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result= await userCollection.deleteOne(query);
+            res.send(result)
         })
     } finally {
         // Ensures that the client will close when you finish/error
