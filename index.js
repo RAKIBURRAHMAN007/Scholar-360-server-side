217d33305887dc66e46e73d818f3a8bf33cd2583
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-01-27 11:51:25 +0600

    vercel beployment completet

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const userCollection = client.db("scholarshipDb").collection("users");
         const allScholarshipCollection = client.db("scholarshipDb").collection("allScholarship");
         const appliedScholarshipCollection = client.db("scholarshipDb").collection("appliedScholarships");
         const reviewCollection = client.db("scholarshipDb").collection("reviews");
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
